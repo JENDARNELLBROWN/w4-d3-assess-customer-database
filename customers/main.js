8072a7c3af15971d5f50c4d99932aeb1a08faf28
Jennifer Brown <jendarnellbrown@gmail.com>
2017-06-14 16:47:50 -0400

    w4-d3-assessment-INCOMPLETE

---
+++ customers/main.js
@@ -0,0 +1,27 @@
+// 1. Fetch your users data
+fetch ("https://tiy-learn-content.s3.amazonaws.com/65ab3f02-users.jpg");
+// 2. Loop over the data
+.then(function(response) {
+          response.json().then(function(customers) {
+      console.log(customers);
+    });
+
+  });
+// 3. Display it in the `.customers` element
+customers {
+  $.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+  }
+});
+}
+
+
+
+  'use strict';
+
+  // Your Code Goes Here
+
+})();
